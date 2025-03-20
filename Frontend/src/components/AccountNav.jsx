0e011d608811f1a78ad20a5fc06df5a535ee7a1f
[Aqib abass] <aqibabass7@gmail.com>
2025-03-20 17:32:39 +0530

    fixed photogid

--- Frontend/src/components/AccountNav.jsx
+++ Frontend/src/components/AccountNav.jsx
@@ -1,44 +1,44 @@
 import { IoMdPerson } from "react-icons/io";
 import { IoListSharp } from "react-icons/io5";
 import { RiHotelLine } from "react-icons/ri";
-import { Link, useLocation } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom";
 
 function AccountNav() {
-
     const { pathname } = useLocation();
     let subpage = pathname.split('/')?.[2];
-    if (subpage === undefined) {
-        subpage = "profile";
-    }
 
-    function linkClasses(type) {
-
-        let classes = ' inline-flex items-center gap-2 py-2 px-6';
-        if (type === subpage) {
-            classes += ' bg-primary text-black font-medium rounded-full';
-        } else {
-            classes += ' text-gray-600 bg-gray-200 rounded-full';
+    function linkClasses(type = null) {
+        if (subpage === undefined) {
+            subpage = 'profile';
         }
+        const isActive = pathname === '/account' && type === 'profile';
+        let classes = 'inline-flex items-center gap-2 py-2 px-4 sm:py-3 sm:px-6 rounded-full text-sm sm:text-base';
+
+        if (type === subpage) {
+            classes += ' bg-primary text-white font-medium';
+        } else {
+            classes += ' bg-gray-200 text-gray-600';
+        }
+
         return classes;
     }
 
     return (
-        <nav className="w-full hidden md:flex justify-center mt-8 gap-2 mb-8">
+        <nav className="w-full flex justify-center mt-8 gap-2 mb-6 items-center flex-wrap">
             <Link className={linkClasses('profile')} to={'/account'}>
-                <IoMdPerson />
-                My Profile
+                <IoMdPerson className="size-6" />
+                <span className="hidden sm:block">My Profile</span>
             </Link>
             <Link className={linkClasses('bookings')} to={'/account/bookings'}>
-                <IoListSharp />
-                My Bookings
+                <IoListSharp className="size-6" />
+                <span className="hidden sm:block">My Bookings</span>
             </Link>
             <Link className={linkClasses('places')} to={'/account/places'}>
-                <RiHotelLine />
-                My Accommodations
+                <RiHotelLine className="size-6" />
+                <span className="hidden sm:block">My Accommodations</span>
             </Link>
         </nav>
-    )
+    );
 }
 
-
-export default AccountNav
+export default AccountNav;
